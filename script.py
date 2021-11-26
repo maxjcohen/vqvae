fb13c3c21792954804733cb62e2ed96fc58f2981
Max Cohen <max.zagouri@pm.me>
2021-11-26 16:03:17 +0100

    Add arguments to parser

--- script.py
+++ script.py
@@ -12,11 +12,6 @@ from vqvae import VQVAE
 import src.metrics as metrics
 from src.utils import export_distances
 
-# Training settings
-EPOCHS = 100
-BATCH_SIZE = 32
-NUM_WORKERS = 4
-
 # Dataset settings
 C = 3
 W = 28
@@ -29,7 +24,7 @@ channel_sizes = [16, 32, DIM_CODEBOOK]
 strides = [2, 2, 1]
 
 
-def get_dataloader(train=True):
+def get_dataloader(train=True, args):
     dataset = datasets.CIFAR10(
         "./datasets/CIFAR10",
         train=train,
@@ -38,13 +33,13 @@ def get_dataloader(train=True):
     )
     return DataLoader(
         dataset=dataset,
-        batch_size=BATCH_SIZE,
+        batch_size=args.batch_size,
         shuffle=train,
-        num_workers=NUM_WORKERS,
+        num_workers=args.num_workers,
     )
 
 
-def train(model, dataloader):
+def train(model, dataloader, args):
     # Traininp loop
     class LITVqvae(pl.LightningModule):
         def __init__(self, model, lr=1e-3):
@@ -56,7 +51,7 @@ def train(model, dataloader):
         def training_step(self, batch, batch_idx):
             images, _ = batch
             encodings = self.model.encode(images)
-            qt = self.model.quanticize(encodings)
+            qt = self.model.quantize(encodings)
             loss_latent = torch.nn.functional.mse_loss(encodings, qt)
             n_uniques = len(qt.permute(0, 2, 3, 1).view(-1, DIM_CODEBOOK).unique(dim=0))
             qt = encodings + (qt - encodings).detach()
@@ -72,7 +67,7 @@ def train(model, dataloader):
 
     # Training
     train_model = LITVqvae(model, lr=1e-3)
-    trainer = pl.Trainer(max_epochs=EPOCHS, gpus=1)
+    trainer = pl.Trainer(max_epochs=args.epochs, gpus=1)
     trainer.fit(train_model, dataloader)
 
     for idx, (loss_name, loss_values) in enumerate(train_model.hist.items()):
@@ -91,6 +86,24 @@ def train(model, dataloader):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="vqvae helper script.")
     parser.add_argument("actions", nargs="+")
+    parser.add_argument(
+        "--epochs",
+        default=100,
+        type=int,
+        help="Number of epochs for training. Default is 100.",
+    )
+    parser.add_argument(
+        "--num-workers",
+        default=1,
+        type=int,
+        help="Number of workers for dataloaders. Default is 1.",
+    )
+    parser.add_argument(
+        "--batch-size",
+        default=16,
+        type=int,
+        help="Batch size for dataloaders. Default is 16.",
+    )
     args = parser.parse_args()
     # Load model
     model = VQVAE(
@@ -100,11 +113,11 @@ if __name__ == "__main__":
         dim_codebook=DIM_CODEBOOK,
         strides=strides,
     )
-    dataloader_train = get_dataloader(train=True)
+    dataloader_train = get_dataloader(train=True, args)
 
     if "train" in args.actions:
-        train(model, dataloader_train)
-        dataloader_val = get_dataloader(train=False)
+        train(model, dataloader_train, args)
+        dataloader_val = get_dataloader(train=False, args)
         reconstruction_cost = metrics.reconstruction(model, dataloader_val)
         print(f"Reconstruction cost: {reconstruction_cost.item()}.")
 
@@ -113,7 +126,7 @@ if __name__ == "__main__":
             model.load_state_dict(torch.load("model.pt"))
         except FileNotFoundError:
             print("Model weights not found, starting training.")
-            train(model, dataloader)
+            train(model, dataloader, args)
         distances = export_distances(model, dataloader_train, reduction="none")
         print(f"Export distances with shape {distances.shape}.")
         torch.save(distances, "distances.pt")
