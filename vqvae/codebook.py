7a6cd514d7dbddd8f0decea4a45e24648b07431f
Max Cohen <max.zagouri@pm.me>
2021-12-07 17:10:58 +0100

    Remove reduction option in compute distance

--- vqvae/codebook.py
+++ vqvae/codebook.py
@@ -6,24 +6,18 @@ class Codebook(torch.nn.Embedding):
         super().__init__(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
         self.weight.data.uniform_(-1 / num_embeddings, 1 / num_embeddings)
 
-    def compute_distances(
-        self, encodings: torch.Tensor, reduction: str = "mean"
-    ) -> torch.Tensor:
+    def compute_distances(self, encodings: torch.Tensor) -> torch.Tensor:
         """Compute distance between encodings and codebooks.
 
         Parameters
         ----------
         encodings: encoding tensor with shape `(B, W, H, C)`.
-        reduction: If `"mean"`, average distances of the codebooks size dimension.
 
         Returns
         -------
-        distances with shape `(B, W, H, N)` if reduction is `"mean"` else `(B, W, H, C,
-        N)`.
+        distances with shape `(B, W, H, N)`.
         """
-        distances = (encodings.unsqueeze(-2) - self.weight).square()
-        if reduction == "mean":
-            distances = distances.mean(-1)
+        distances = (encodings.unsqueeze(-2) - self.weight).square().mean(-1)
         return distances
 
     def codebook_lookup(self, quantized: torch.Tensor) -> torch.Tensor:
