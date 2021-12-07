515c3431a7194f9c20655fa1a895adf029056bdc
Max Cohen <max.zagouri@pm.me>
2021-12-07 16:38:25 +0100

    Export batch per batch

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,9 @@ from tqdm import tqdm
 
 @torch.no_grad()
 def export_distances(model, dataloader, reduction="mean"):
-    distances = []
     for images, _ in tqdm(dataloader, total=len(dataloader)):
         encodings = model.encode(images)
-        distances.append(
-            model.emb.compute_distances(
-                encodings.permute(0, 2, 3, 1), reduction=reduction
-            )
+        distances = model.emb.compute_distances(
+            encodings.permute(0, 2, 3, 1), reduction=reduction
         )
-    distances = torch.cat(distances, dim=0)
-    return distances
+        print(distances.shape)
