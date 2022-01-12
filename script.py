4e22074133328e9a9fe72eba49e1bbc60488767d
Max Cohen <max.zagouri@pm.me>
2022-01-12 09:38:34 -0500

    Remove wrapping function for parser

--- script.py
+++ script.py
@@ -22,40 +22,32 @@ except ImportError:
 print(f"Using logger {logger}.")
 
 
-def configure_parser():
-    parser = argparse.ArgumentParser(description="vqvae helper script.")
-    parser.add_argument("actions", nargs="+")
-    parser.add_argument(
-        "--epochs",
-        default=100,
-        type=int,
-        help="Number of epochs for training. Default is 100.",
-    )
-    parser.add_argument(
-        "--num-workers",
-        default=1,
-        type=int,
-        help="Number of workers for dataloaders. Default is 1.",
-    )
-    parser.add_argument(
-        "--batch-size",
-        default=16,
-        type=int,
-        help="Batch size for dataloaders. Default is 16.",
-    )
-    parser.add_argument(
-        "--num-codebook",
-        default=256,
-        type=int,
-        help="Number of codebooks.",
-    )
-    parser.add_argument(
-        "--dim-codebook",
-        default=32,
-        type=int,
-        help="Dimension of codebook vectors.",
-    )
-    return parser
+parser = argparse.ArgumentParser(description="vqvae helper script.")
+parser.add_argument("actions", nargs="+")
+parser.add_argument(
+    "--epochs",
+    default=100,
+    type=int,
+    help="Number of epochs for training. Default is 100.",
+)
+parser.add_argument(
+    "--num-workers",
+    default=1,
+    type=int,
+    help="Number of workers for dataloaders. Default is 1.",
+)
+parser.add_argument(
+    "--batch-size",
+    default=16,
+    type=int,
+    help="Batch size for dataloaders. Default is 16.",
+)
+parser.add_argument(
+    "--num-codebook", default=256, type=int, help="Number of codebooks."
+)
+parser.add_argument(
+    "--dim-codebook", default=32, type=int, help="Dimension of codebook vectors."
+)
 
 
 def get_dataloader(args, train=True):
