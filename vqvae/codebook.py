783e7ce61bef7735bd28f69ac05652d66cc163bb
Max Cohen <max.zagouri@pm.me>
2022-01-29 11:10:09 +0100

    Remove batch mention in codebook docs

--- vqvae/codebook.py
+++ vqvae/codebook.py
@@ -14,7 +14,7 @@ class Codebook(torch.nn.Embedding):
 
         Parameters
         ----------
-        encoding: input tensor with shape `(B, *, D)`.
+        encoding: input tensor with shape `(*, D)`.
 
         Returns
         -------
@@ -30,27 +30,27 @@ class Codebook(torch.nn.Embedding):
 
         Parameters
         ----------
-        encodings: encoding tensor with shape `(B, *, D)`.
+        encodings: encoding tensor with shape `(*, D)`.
 
         Returns
         -------
-        distances with shape `(B, *, K)`.
+        Distances with shape `(*, K)`.
         """
         distances = (encodings.unsqueeze(-2) - self.weight).square().mean(-1)
         return distances
 
-    def codebook_lookup(self, quantized: torch.Tensor) -> torch.Tensor:
-        """Associate vectors in the quantized tensor with the corresponding codebook.
+    def codebook_lookup(self, indices: torch.Tensor) -> torch.Tensor:
+        """Associate tensor of indices with the corresponding codebooks.
 
         Parameters
         ----------
-        quantized: Discrete vector with shape (B, *).
+        indices: Discrete vector with shape (*).
 
         Returns
         -------
-        Tensor of codebooks with shape `(B, *)`.
+        Tensor of codebooks with shape `(*, D)`.
         """
-        return self(quantized)
+        return self(indices)
 
     @property
     def num_codebook(self):
