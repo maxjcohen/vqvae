5508e17969b45da9585ded2aab10b164edf51b14
Max Cohen <max.zagouri@pm.me>
2022-02-02 07:55:10 -0500

    Add oze training script, with figure logging

---
+++ scripts/dataset/oze.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import datetime
+
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from vqvae.vqvae import OzeVQVAE
+
+from src.trainers.oze import OzeTrainer
+from oze.datamodule import OzeDataModule
+from ..utils import parser, get_logger
+
+
+exp_name = "oze-ozedata"
+
+# Load model
+dim_codebook = 32
+num_codebook = 256
+model = OzeVQVAE(
+    num_codebook=num_codebook,
+    dim_codebook=dim_codebook,
+)
+
+
+def get_datamodule(args):
+    return OzeDataModule(
+        dataset_path="datasets/oze/data_2020_2021.csv",
+        T=24*7,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+    )
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    args.lr = args.lr or 1e-3
+    litmodule = OzeTrainer(model, lr=args.lr)
+
+    # Load logger
+    logger = get_logger(exp_name)
+    logger.experiment["hparams"] = vars(args)
+
+    # Define checkpoints
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=Path("checkpoints") / exp_name,
+        filename=f"{datetime.datetime.now().strftime('%Y_%m_%d__%H%M%S')}",
+        monitor="val_reconstruction_loss",
+        save_last=True,
+    )
+
+    # Load trainer
+    trainer = pl.Trainer(
+        max_epochs=args.epochs,
+        gpus=args.gpus,
+        logger=logger,
+        callbacks=[checkpoint_callback],
+    )
+
+    # Load dataset
+    datamodule = get_datamodule(args)
+
+    # Train
+    trainer.fit(litmodule, datamodule=datamodule)
--- src/trainers/oze.py
+++ src/trainers/oze.py
@@ -5,6 +5,22 @@ import plotly.graph_objects as go
 from aim import Figure
 
 
+def aim_fig_plot_ts(arrays):
+    fig = go.Figure()
+    for name, array in arrays.items():
+        fig.add_trace(
+            go.Scatter(
+                y=array.detach().cpu().numpy().squeeze(), mode="lines", name=name
+            )
+        )
+    fig.update_layout(legend=dict(xanchor="left", x=0.5))
+    return Figure(fig)
+
+
+def flatten_ts_batch(batch):
+    return torch.cat([sample for sample in batch.transpose(0, 1)], dim=0)
+
+
 class OzeTrainer(pl.LightningModule):
     def __init__(self, model, lr=1e-3):
         super().__init__()
@@ -27,6 +43,28 @@ class OzeTrainer(pl.LightningModule):
         reconstructions = self.model(u)
         reconstruction_loss = F.mse_loss(reconstructions, y)
         self.log("val_reconstruction_loss", reconstruction_loss)
+        if batch_idx == 0:
+            self.logger.experiment.track(
+                aim_fig_plot_ts(
+                    {"observations": y[:, 1], "predictions": reconstructions[:, 1]}
+                ),
+                name="batch-comparison",
+                epoch=self.current_epoch,
+                context={"subset": "val"},
+            )
+        return y, reconstructions
+
+    def validation_epoch_end(self, outputs):
+        originals = torch.cat([flatten_ts_batch(batch) for batch, _ in outputs], dim=0)
+        predictions = torch.cat(
+            [flatten_ts_batch(batch) for _, batch in outputs], dim=0
+        )
+        self.logger.experiment.track(
+            aim_fig_plot_ts({"observations": originals, "predictions": predictions}),
+            name="full-comparison",
+            epoch=self.current_epoch,
+            context={"subset": "val"},
+        )
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.parameters(), self.lr)
