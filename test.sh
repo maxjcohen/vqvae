9e5895aa16f291eb22c797711d3d541a296e07f4
Max Cohen <max.zagouri@pm.me>
2022-03-15 10:56:09 -0400

    Add test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+EPOCHS=5
+LR=1e-3
+NUM_WOKERS=4
+GPUS=1
+
+for flavor in classic ema gumbel; do
+    python -m scripts.dataset.cifar \
+	--gpus $GPUS \
+	--num-workers $NUM_WOKERS \
+	--epochs $EPOCHS \
+	--lr $LR \
+	--flavor $flavor
+done
