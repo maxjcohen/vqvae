8dfdfc4cdffac488a9dec4a6ea4bb8b5ab89f9b0
Max Cohen <max.zagouri@pm.me>
2022-05-31 12:21:38 +0200

    Fix minor coding errors

--- src/model/miniimagenet.py
+++ src/model/miniimagenet.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from vqvae.codebook import Codebook, EMACodebook, GumbelCodebook
 from .cifar import CifarVQVAE
 from .backbone import MiniimagenetAutoEncoder
+
 
 class MiniImagenetVQVAE(CifarVQVAE):
     def __init__(
@@ -9,14 +12,7 @@ class MiniImagenetVQVAE(CifarVQVAE):
         dim_codebook: int,
         codebook_flavor: Optional[str] = "ema",
     ):
-        super().__init__()
+        super().__init__(num_codebook=num_codebook, dim_codebook=dim_codebook)
         self.autoencoder = MiniimagenetAutoEncoder(out_channels=dim_codebook)
         self.encode = self.autoencoder.encode
         self.decode = self.autoencoder.decode
-        CodebookFlavor = {
-            "classic": Codebook,
-            "ema": EMACodebook,
-            "gumbel": GumbelCodebook,
-        }[codebook_flavor]
-        self.codebook = CodebookFlavor(num_codebook, dim_codebook)
-        self.codebook_flavor = codebook_flavor
