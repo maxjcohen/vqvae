b3dd529a3d76237f63cfd3a710a0e1e6c6ea071b
Max Cohen <max.zagouri@pm.me>
2022-06-01 16:32:16 +0200

    Fix export script

--- scripts/export_weights.py
+++ scripts/export_weights.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import torch
 
-from src.trainer.cifar import LitCifarTrainer
+from src.trainer.cifar import LitTrainer
 
 
 parser = argparse.ArgumentParser(
@@ -13,5 +13,5 @@ parser.add_argument("checkpoint", type=Path, help="Pytorch Lightning checkpoint 
 parser.add_argument("save_path", type=Path, help="Path to save the model's state dict.")
 args = parser.parse_args()
 
-litmodule = LitCifarTrainer.load_from_checkpoint(args.checkpoint)
+litmodule = LitTrainer.load_from_checkpoint(args.checkpoint)
 torch.save(litmodule.model.state_dict(), args.save_path)
